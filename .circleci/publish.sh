d7dedefd81a8e0f914046046dbdf60d31049af98
Norman Walsh <ndw@nwalsh.com>
2021-06-13 11:28:02 +0100

    Attempt to fix CI build issue

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -5,6 +5,9 @@ REPO="git@github.com:${CIRCLE_PROJECT_USERNAME}/xmlresolver.org.git"
 
 echo "Publishing to gh-pages in $REPO"
 
+mkdir $HOME/staging
+cd $HOME/staging
+
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_USER
 git clone --quiet --branch=gh-pages "$REPO" gh-pages
@@ -19,4 +22,9 @@ fi;
 rsync -ar --delete $BUILD/website/ ./
 git add --all .
 git commit -m "CircleCI build: $CIRCLE_BUILD_URL"
-git push
+
+pwd
+ls -lA
+
+#git push -fq origin gh-pages
+
