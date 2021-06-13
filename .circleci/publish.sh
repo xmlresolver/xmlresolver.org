a67ee6871ec27d2f289893a4820135d4f8241121
Norman Walsh <ndw@nwalsh.com>
2021-06-13 10:51:39 +0100

    Working on setting up website publication

---
+++ .circleci/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BUILD=`pwd`/build
+REPO="git@github.com:${CIRCLE_PROJECT_USERNAME}/xmlresolver.org.git"
+
+echo "Publishing to gh-pages in $REPO"
+
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USER
+git clone --quiet --branch=gh-pages "$REPO" gh-pages
+
+cd gh-pages
+
+# Set this in the CircleCI "Settings/Environment Variables" section
+if [ "$GITHUB_CNAME" != "" ]; then
+    echo $GITHUB_CNAME > CNAME
+fi;
+
+rsync -ar --delete $BUILD/website/ ./
+git add --all .
+git commit -m "CircleCI build: $CIRCLE_BUILD_URL"
+ls -l
+#git push -fq origin gh-pages
